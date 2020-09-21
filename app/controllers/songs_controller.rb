e8bb4107efd0a788581ee35928d7e3af8266c42c
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-09-21 07:35:08 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
   def index
     if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
+      @artist = Artist.find(params[:artist_id])
       if @artist.nil?
         redirect_to artists_path
       else
@@ -14,7 +14,7 @@ class SongsController < ApplicationController
 
   def show
     if params[:artist_id]
-      @artist = Artist.find_by(id: params[:artist_id])
+      @artist = Artist.find(params[:artist_id])
       @song = @artist.songs.find_by(id: params[:id])
       if @song.nil?
         redirect_to artist_songs_path(@artist)
